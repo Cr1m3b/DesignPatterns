2cb18d8f9f204cfe019462f8ab02c9e0e4f8be2f
Cr1m3b <iosifvasoul@yahoo.gr>
2022-03-01 19:35:31 +0100

    Add DP

---
+++ MapaFactory/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choise = "1";
+            if (choise == "1")
+            {
+                MasterCard m = new MasterCard();
+                m.GetCardType();
+            }
+            else if (choise == "2")
+            {
+                VisaCard v = new VisaCard();
+                v.GetCardType();
+            }
+            else if (choise == "3")
+            {
+                PaySafeCard p = new PaySafeCard();
+                p.GetCardType();
+            }
+
+        }
+
+        class MasterCard
+        {
+            public string GetCardType()
+            {
+                return "Master Card.";
+            }
+            public int GetCreditLimit()
+            {
+                return 35000;
+            }
+        }
+        class VisaCard
+        {
+            public string GetCardType()
+            {
+                return "Visa Card.";
+            }
+            public int GetCreditLimit()
+            {
+                return 25000;
+            }
+        }
+        class PaySafeCard
+        {
+            public string GetCardType()
+            {
+                return "Pay Safe Card.";
+            }
+            public int GetCreditLimit()
+            {
+                return 15000;
+            }
+        }
+
+
+    }
+}
---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICard card = SimpleFactory("1");
+            Console.WriteLine(card.GetCardType());
+            Console.WriteLine(card.GetCreditLimit());
+        }
+
+        public static ICard SimpleFactory(string choise)
+        {
+            switch (choise)
+            {
+                case "1": return new MasterCard();
+                case "2": return new VisaCard();
+                case "3": return new PaySafeCard();
+                default: return null;
+            }
+        }
+    }
+
+    interface ICard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+    }
+    class MasterCard : ICard
+    {
+        public string GetCardType() { return "Master Card."; }
+        public int GetCreditLimit() { return 35000; }
+    }
+    class VisaCard : ICard
+    {
+        public string GetCardType() { return "Visa Card."; }
+        public int GetCreditLimit() { return 25000; }
+    }
+    class PaySafeCard : ICard
+    {
+        public string GetCardType() { return "Pay Safe Card."; }
+        public int GetCreditLimit() { return 15000; }
+    }
+}
